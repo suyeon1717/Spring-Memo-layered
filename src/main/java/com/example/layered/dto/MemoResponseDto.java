96793fb1871dffeebdf248365014049816ce2a91
고수연 <sy@gosuyeon-ui-MacBookPro.local>
2024-12-05 20:59:02 +0900

    초기 클래스 생성

---
+++ src/main/java/com/example/layered/dto/MemoResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.layered.dto;
+
+import com.example.layered.entity.Memo;
+
+public class MemoResponseDto {
+
+    private Long id;
+    private String title;
+    private String contents;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.title = memo.getTitle();
+        this.contents = memo.getContents();
+    }
+}
